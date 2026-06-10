2f9e9fa5fa2611acaf4bd67d2686e17bf59971d3
f1oating <dtoronchenko@gmail.com>
2026-06-10 14:31:19 +0100

    RenderGraph

--- Examples/Common/include/Common/RenderGraph.h
+++ Examples/Common/include/Common/RenderGraph.h
@@ -1,23 +1,82 @@
 #pragma once
 
 #include <cstdint>
+#include "Mad-RHI/Device.h"
+#include <functional>
+#include <string>
+#include <vector>
 
 namespace mad::common {
 
-struct RGTextureHandle
+using RGTextureHandle = uint16_t;
+
+struct RGTextureDesc
 {
-    uint16_t Index;
-    uint16_t Version;
+    rhi::TextureDimension Dimension = rhi::TextureDimension::Texture2D;
+    uint32_t Width = 0;
+    uint32_t Height = 0;
+    union 
+    {
+        uint32_t ArraySize = 1;
+        uint32_t Depth; 
+    };
+    rhi::TextureFormat Format = rhi::TextureFormat::R8G8B8A8_UNorm;
+    uint32_t MipLevels = 1;
+    uint32_t SampleCount = 1;
 };
 
-struct RGBufferHandle
+struct RGTextureEntry
 {
-    uint16_t Index;
-    uint16_t Version;
+    RGTextureDesc Desc;
+    uint8_t ComputedBindFlags;
+    rhi::RefPtr<rhi::Texture> PhysicalTexture;
 };
+
+struct RGPass
+{
+    std::string Name;
+    std::function<void(rhi::CommandQueue*)> Execute;
+    std::vector<RGTextureEntry> Reads;
+    std::vector<RGTextureEntry> Writes;
+};
+
+class RGPassBuilder;
 
 class RenderGraph
 {
+friend class RGPassBuilder;
+
+public:
+    RenderGraph(rhi::Device* device);
+
+    void AddPass(std::string name, std::function<void(rhi::CommandQueue*)> execute,
+        std::function<void(RGPassBuilder&)> setup);
+
+    void Compile();
+    void Execute(rhi::CommandQueue* queue);
+
+    RGTextureHandle CreateTexture(RGTextureDesc desc);
+
+private:
+    rhi::Device* m_Device = nullptr;
+
+    std::vector<RGTextureEntry> m_Textures;
+    std::vector<RGPass> m_Passes;
+
+};
+
+class RGPassBuilder
+{
+public:
+    RGPassBuilder(RenderGraph& rg, RGPass& pass);
+
+    rhi::TextureView* GetTextureSRV(RGTextureHandle handle);
+    rhi::TextureView* GetTextureRTV(RGTextureHandle handle);
+    rhi::TextureView* GetTextureDSV(RGTextureHandle handle);
+
+private:
+    RenderGraph m_RG;
+    RGPass m_Pass;
 
 };
 
--- Examples/Common/src/Common/RenderGraph.cpp
+++ Examples/Common/src/Common/RenderGraph.cpp
@@ -2,4 +2,89 @@
 
 namespace mad::common {
 
+RGPassBuilder::RGPassBuilder(RenderGraph& rg, RGPass& pass)
+    : m_RG(rg), m_Pass(pass)
+{
+
+}
+
+rhi::TextureView* RGPassBuilder::GetTextureSRV(RGTextureHandle handle)
+{
+    RGTextureEntry entry = m_RG.m_Textures[handle];
+
+    entry.ComputedBindFlags |= rhi::RESOURCE_BIND_SHADER_RESOURSE;
+    m_Pass.Reads.push_back(entry);
+
+    return entry.PhysicalTexture->GetDefaultSRV().Get();
+}
+
+rhi::TextureView* RGPassBuilder::GetTextureRTV(RGTextureHandle handle)
+{
+    RGTextureEntry entry = m_RG.m_Textures[handle];
+
+    entry.ComputedBindFlags |= rhi::RESOURCE_BIND_RENDER_TARGET;
+    m_Pass.Writes.push_back(entry);
+
+    return entry.PhysicalTexture->GetDefaultRTV().Get();
+}
+
+rhi::TextureView* RGPassBuilder::GetTextureDSV(RGTextureHandle handle)
+{
+    RGTextureEntry entry = m_RG.m_Textures[handle];
+
+    entry.ComputedBindFlags |= rhi::RESOURCE_BIND_DEPTH_STENCIL;
+    m_Pass.Writes.push_back(entry);
+
+    return entry.PhysicalTexture->GetDefaultDSV().Get();
+}
+
+void RenderGraph::AddPass(std::string name, std::function<void(rhi::CommandQueue*)> execute,
+    std::function<void(RGPassBuilder&)> setup)
+{
+    RGPass pass;
+    pass.Name = name;
+    pass.Execute = execute;
+    
+    RGPassBuilder builder(*this, pass);
+    setup(builder);
+
+    m_Passes.push_back(pass);
+}
+
+void RenderGraph::Compile()
+{
+    for (RGTextureEntry entry : m_Textures)
+    {
+        rhi::TextureDesc desc;
+        desc.Dimension = entry.Desc.Dimension;
+        desc.Width = entry.Desc.Width;
+        desc.Height = entry.Desc.Height;
+        desc.ArraySize = entry.Desc.ArraySize;
+        desc.Depth = entry.Desc.Depth;
+        desc.Format = entry.Desc.Format;
+        desc.MipLevels = entry.Desc.MipLevels;
+        desc.SampleCount = entry.Desc.SampleCount;
+        desc.BindFlags = entry.ComputedBindFlags;
+
+        m_Device->CreateTexture(entry.PhysicalTexture.GetAddress(), desc);
+    }
+}
+
+void RenderGraph::Execute(rhi::CommandQueue* queue)
+{
+    for (RGPass pass : m_Passes)
+    {
+        pass.Execute(queue);
+    }
+}
+
+RGTextureHandle RenderGraph::CreateTexture(RGTextureDesc desc)
+{
+    RGTextureHandle handle = m_Textures.size();
+
+    m_Textures.push_back({ .Desc = desc });
+
+    return handle;
+}
+
 }
